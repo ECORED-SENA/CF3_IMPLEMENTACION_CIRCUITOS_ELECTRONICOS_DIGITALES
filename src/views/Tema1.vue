2a8715d1ba643432b307a75833747bbe3cfdff3a
Andrea Botello <andrea.botellor@misena.edu.co>
2022-03-30 18:08:34 -0500

    updated

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -9,10 +9,51 @@
   
     p.mb-4(data-aos="fade") Un paso importante cuando se están analizando circuitos digitales es el proceso de medición, porque no solo se limita a tomar valores con un instrumento, sino que va mucho más allá, tal como lo presenta la siguiente figura.  
 
+    .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+      p.mb-0 <b>Figura 1</b> Aspectos del proceso de medición
+
+    .row.justify-content-center.mb-5
+      .col-md-4(data-aos="fade-right")
+        img(src='@/assets/curso/tema1-img.svg' alt='Aspectos del proceso de medición')
+      .col-md-7(data-aos="fade-left")
+        h4.ps-md-5 Aspectos del proceso de medición
+        LineaTiempoE.color-acento-contenido
+          .row.bg-azul.r-10.p-4
+            p.mb-0 Tomar valores con un instrumento.
+          .row.bg-morado.r-10.p-4
+            p.mb-0 Realizar una buena interpretación de estos datos, aplicando el álgebra de Boole, teoremas y leyes, y las reglas para el análisis.
+          .row.bg-azul3.r-10.p-4
+            p.mb-0 Tener en cuenta las especificaciones de los elementos inmersos en las mediciones, estos datos son suministrados por los fabricantes en los datasheet, definen los parámetros y características de estos componentes, así como las condiciones óptimas de operación. Es por eso que siempre se debe tener a la mano las hojas de datos de componentes y equipos.
+
+    .row.justify-content-center.cajon-amarillo.cajon.color-primario.p-4.mb-4.mx-1(data-aos="fade-down")
+      .col-md-1.p-0.mb-4.mb-md-0
+        img(src='@/assets/curso/tema1-icono.svg' alt='Icono')
+      .col-md-9
+        p.mb-0 Las normas aplicables a los procesos de medición también deben ser tenidas en cuenta, pues estas buscan proteger la vida y preservar el medio ambiente. Es importante consultarlas y aplicarlas en el desarrollo de los procesos de medición.
+
+    p.mb-4(data-aos="fade") Ahora bien, para desarrollar este tipo de procedimientos es necesario recordar y comprender los conceptos que hay alrededor del mismo. Entre ellos, la magnitud eléctrica, la cual se refiere a aquellos procesos físicos asociados al movimiento de electrones y que pueden ser controlados, aprovechados y por lo tanto, se pueden medir. A continuación, se presentan los tipos de magnitudes eléctricas.
 
 
 
-    
+
+
+    .row.justify-content-center.cajon.color-acento-contenido.p-4.mx-1(data-aos="fade-down")
+      .col-md-10
+        p.mb-0 En sistemas digitales estas mediciones se centran en determinar tensiones, corrientes muy bajas y también resistencias para limitar y fijar valores. Dentro del análisis circuital son escasas las veces donde se hace necesario recurrir a análisis complejos, siendo suficiente en la mayoría de los casos, la aplicación de la Ley de Ohm.
+
+    Separador
+
+
+    #t_1_1
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 1.1 Equipos
+
+    p.mb-4(data-aos="fade") Medir es comparar una magnitud con una unidad preestablecida, la cual está referida a un patrón de medición, que es el que da las pautas para determinar la naturaleza de la medición.  Los instrumentos utilizados en cada caso es un aspecto importante a tener en cuenta en estos procesos. Algunos, en ocasiones, pueden sufrir averías si no se utilizan de forma adecuada.
+
+
+
+
+
 </template>
 
 <script>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -44,7 +44,7 @@
 
     p.mb-4(data-aos="fade") Los métodos de detección de fallas siguen los mismos conceptos aplicados en la mayoría de resolución de problemas. En general se deben seguir estos pasos:
 
-    TabsA.color-acento-contenido.mb-5
+    TabsA.color-acento-contenido.mb-4
       .row.tarjeta.bg-azul2.r-10.p-4(titulo="Detección de la falla")
         .col-md-12
           p.mb-4 Aplicar los procedimientos ya conocidos para hacerle seguimiento al problema, aplicando la lógica y el análisis, hasta encontrar el origen del problema.
@@ -53,15 +53,68 @@
         .col-md-6
           p Por ejemplo, se requiere determinar el comportamiento de la salida  en el circuito de la siguiente figura, nótese que aunque es un circuito combinacional la entrada está alimentada por una señal cambiante, es decir, una señal de pulsos, también conocida como señal de reloj, utilizada más que todo en circuitos secuenciales. Esa señal puede hacer que la salida también sea cambiante en el tiempo, entonces eso es lo primero que se debe determinar. 
         .col-md-12
+          .wrap-player
+            audio(controls='')(style= "width: 100%")
+              //source(src='../../public/downloads/Arquitectura.mp3' type='audio/mp3')
+              //|  Your browser does not support the audio tag
+      .tarjeta.bg-azul2.r-10.p-4(titulo="Aislar el problema")
+        h4 Aislar el problema
+        p Una vez detectado el problema se debe aislar, es decir, verificar que sea solo este y que los circuitos asociados estén en funcionamiento. 
+        p Algunas veces es necesario utilizar un par, es decir, uno que haga la misma función y esté operando normalmente. Será necesario intercambiar elementos, siempre y cuando no represente mucho riesgo para el componente,  el montaje y desmontaje de estos.
+      .tarjeta.bg-azul2.r-10.p-4(titulo="Corregir el problema")
+        h4 Corregir el problema
+        p Finalmente, se debe reemplazar el componente dañado y verificar su funcionamiento óptimo, algunas veces el daño de un componente repercute en otros, afectando su funcionamiento normal. 
 
-
-        
-    .row.justify-content-center.cajon.color-acento-contenido.p-4.mb-4.mx-1
+    .row.justify-content-center.cajon.color-acento-contenido.p-4.mb-4
       .col-md-10
         p.mb-0 Las fallas más comunes en los componentes electrónicos se debe al mal funcionamiento de la circuitería interna, también influyen las entradas o salidas en cortocircuito con conexión a  GND  o Vcc y entradas o salidas en circuito abierto o cortocircuito entre dos terminales (diferentes de la GND y Vcc).
 
+    Separador
 
 
+    #t_2_2
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 2.2. Equipos y <em>software</em> de diagnóstico
+
+    .row.justify-content-center
+      .col-md-10
+        .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+          p.mb-0 Equipo de diagnóstico avanzado, tomado de Cedesa (s.f.).
+
+    .row.justify-content-center.mb-3
+      .col-md-6.mb-4.mb-md-0(data-aos="fade-right")
+        img.mb-3(src='@/assets/curso/tema2-2-img.png' alt='Equipo de diagnóstico avanzado, tomado de Cedes')
+      .col-md-6(data-aos="fade-left")
+        p Además de los equipos ya mencionados, los que generalmente ofrecen las prestaciones necesarias para la mayoría de los problemas presentados en circuitería digital, existen otros equipos de diagnóstico avanzado, los cuales son más que todo utilizados para diagnosticar tarjetas electrónicas, que generalmente necesitan de un par, es decir, una tarjeta en buen estado, que sirve para establecer unos parámetros, que luego serán comparados en la tarjeta problemática.
+        p Contar con un equipo de esta tecnología puede ser ventajoso, debido a que basan su funcionamiento en la utilización de un par o la predicción del funcionamiento de ciertos componentes, una persona con habilidades en reparación puede lograr buenos diagnósticos, utilizando equipos más simples como analizadores lógicos y puntas de pruebas.
+
+    .row.mb-5
+      .col-md-1
+      .col-md-5(data-aos="fade")
+          p.mb-0 Estos equipos son costosos y normalmente se encuentran en empresas de alta producción.
+
+    .tarjeta.bg-degrade-azul.p-3.mb-5.py-md-4(data-aos="fade-down")
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-2
+          img.centrar-img(src="@/assets/curso/tema3-1-ico.svg" style="width: 91px;")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0.text-white
+              p.mb-0 Para complementar el estudio del uso de este dispositivo, se invita a ver el video “Tutorial analizador lógico USB”, que se encuentra en el material complementario.  
+            .col-sm-auto.me-md-5.text-blue
+              a.boton.color-acento-botones(href="https://www.youtube.com/watch?v=3ISKULwwU2M" target="_blank")
+                span.text-blue Ver video
+                i.fas.fa-play.text-blue
+
+    .row.justify-content-center.mb-3
+      .col-md-6.mb-4.mb-md-0(data-aos="fade-right")
+        p El <em>software</em> de diagnóstico, por su parte, se constituye en una herramienta útil para el análisis de circuitos. Generalmente viene asociado a las herramientas ya mencionadas, analizadores lógicos y a las de diagnóstico avanzado. 
+        p Los equipos que cuentan con un sistema de cómputo integran el <em>software</em> de diagnóstico, es decir, el POST. Aunque se puede complementar con <em>software</em> externo para un diagnóstico completo, un ejemplo de esto puede ser la herramienta que viene incluida en el POST de las PC para diagnosticar el disco duro, pero se puede complementar con una herramienta como defrag para un diagnóstico avanzado del mismo, sin incluir más herramientas.
+      .col-md-6(data-aos="fade-left")
+        img(src='@/assets/curso/tema2-2-img2.png' alt='Equipo de diagnóstico avanzado, tomado de Cedes')
+        .titulo-sexto.color-acento-contenido.mt-n4
+          p.mb-0 Software de Elvis, captura de pantalla
+        p Algunas plataformas educativas incluyen este tipo de software para pruebas de montajes, es decir, se pueden hacer pruebas de circuitos, inyectando señales generadas por el programa y esperando unos resultados; Elvis de National Instruments es un ejemplo.
 
 
 </template>
